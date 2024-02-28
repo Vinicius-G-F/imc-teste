2f744626f0dff75a0f4f666020f673eef8398167
Kelvin  Ryan <90951461+KelvinMoreira@users.noreply.github.com>
2024-02-28 20:12:24 -0300

    js
    ddd

---
+++ imc-calculator/index.js
@@ -0,0 +1,20 @@
+const calcular = window.document.querySelector('#botaocalcular')
+
+function Calcular(){
+    const nomebruto = window.document.querySelector('#nome')
+    const alturabruta = window.document.querySelector('#altura')
+    const pesobruto = window.document.querySelector('#peso')
+    const resultado = window.document.querySelector('#respostaarea')
+
+    if (nomebruto, alturabruta, pesobruto !== ''){
+        let nome = nomebruto.value
+        let altura = Number(alturabruta.value)
+        let peso = Number(pesobruto.value)
+        let divisao1 = peso / altura
+        let divisao2 = divisao1 / altura
+        let valorfinal = Number(divisao2.toFixed(2))
+        resultado.innerHTML = `Olá ${nome}, o seu peso atual é ${peso}kg e o seu imc é ${valorfinal}`
+    }else{
+    
+    }
+}
